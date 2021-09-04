2597e4c6005fcc94573d3b777b9d048cbefcb6aa
ImDarkDiamond <admin@arcanebot.xyz>
2021-09-04 11:30:42 -0600

    fix: remove prepending /

--- src/main.rs
+++ src/main.rs
@@ -218,7 +218,7 @@ fn normalize_path(request_path: &str) -> (&str, &str) {
         if let Some(maybe_api_version) = trimmed_path.split('/').nth(1) {
             if let Some(version_number) = maybe_api_version.strip_prefix('v') {
                 if version_number.parse::<u8>().is_ok() {
-                    let len = "/api/v".len() + version_number.len();
+                    let len = "/api/v".len() + version_number.len() + 1;
                     return (&request_path[..len], &request_path[len..]);
                 };
             };
